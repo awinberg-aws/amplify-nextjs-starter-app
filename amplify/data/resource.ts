1b101eac58eb9ff2240af0f295aa362bc71b745f
Albert Winberg <winberga@amazon.com>
2024-03-20 20:38:39 +0000

    more models

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -5,6 +5,23 @@ const schema = a.schema({
     content: a.string(),
     isDone: a.boolean(),
   }).authorization([a.allow.private('iam')]),
+  House: a.model({
+    address: a.string().required(),
+    bedrooms: a.integer().required(),
+    bathrooms: a.float().required(),
+    stories: a.integer().required(),
+    squareFeet: a.integer().required(),
+    hasYard: a.boolean().required
+  }).authorization([a.allow.private('iam')]),
+  Person: a.model({
+    name: a.string().required(),
+    age: a.integer().required(),
+    primaryResidence: a.hasOne('House'),
+  }).authorization([a.allow.private('iam')]),
+  TaskList: a.model({
+    name: a.string().required(),
+    todos: a.hasMany('Todo'),
+  }).authorization([a.allow.private('iam')]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
